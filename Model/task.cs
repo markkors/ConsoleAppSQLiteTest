b9d40a16a61276463dec1449a4d68c2ccbae677c
markk <m.kors@rocmn.nl>
2024-06-03 09:03:13 +0200

    Add project files.

---
+++ Model/task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSQLiteTest.Model
+{
+    public class task
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using Model;
+
+
+namespace ConsoleAppSQLiteTest
+{
+    internal class Program
+    {
+
+
+        public static SQLiteDb dbs;
+        
+        static void Main(string[] args)
+        {
+
+            
+            Create();
+            // wait for key press
+            Console.ReadKey();
+
+        }
+
+        private static void Create()
+        {
+            
+            dbs = new SQLiteDb("mydb.db");
+            // create a new instance of the SQLiteDb class
+            List<Model.task> tasks = dbs.readTasks("task");
+            dbs.exportTableJSON(tasks);
+        }
+
+        private void test()
+        {
+            // niets
+        }
+    }
+}
